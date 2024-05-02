6934800250b0b376a720c9bf740ff79853b14122
Cristian Martinez <mrkittmm@gmail.com>
2024-05-02 13:52:17 -0300

    installe firebase y comence a trabajar en itemCount

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { initializeApp } from "firebase/app";
+
+
 import App from './App.jsx'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXOMe-5k4YwCYTQmxTyk8BNg1eBIM8yoc",
+  authDomain: "vivirfitness-1f4ad.firebaseapp.com",
+  projectId: "vivirfitness-1f4ad",
+  storageBucket: "vivirfitness-1f4ad.appspot.com",
+  messagingSenderId: "559705081854",
+  appId: "1:559705081854:web:10f04a4cf13b6495fe22af"
+};
+
+initializeApp(firebaseConfig);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
