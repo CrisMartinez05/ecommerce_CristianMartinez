0306ebef06264b7dd85d623862b5b4c13a7dff5e
Cristian Martinez <mrkittmm@gmail.com>
2024-05-03 17:36:16 -0300

    agrego clases alal App.css

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,24 +1,24 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
-import CardGroup from 'react-bootstrap/CardGroup';
+
 
 export const Item = ({ product }) => (
   <Card style={{ width: "18rem" }}>
-    <Card.Img variant="top" src={product.image} />
+    <Card.Img variant="top" src={product.image} alt={product.image} />
     <Card.Body>
       <Card.Title>{product.title}</Card.Title>
+      <br />
       <Card.Title>
         <span>$ </span>
         {product.price}
       </Card.Title>
     </Card.Body>
-    <Card.Footer>
-          <Link to={`/item/${product.id}`}>
-          <Button variant="primary">Ver Detalle</Button>
-          </Link>          
-    </Card.Footer>
-   </Card>
-  
-  
+    <div className="verDetalle">
+      <Link to={`/item/${product.id}`}>
+        <Button variant="dark">Ver Detalle</Button>
+      </Link>
+    </div>
+    <br />
+  </Card>
 );
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,7 +2,7 @@ import { Item } from "./Item";
 
 export const ItemList = ({ products }) => {
   return (
-    <div className="d-flex align-items-stretch justify-content-center" style={{flexWrap:'wrap'}}>
+    <div className="itemList">
       {products.map((product) => (
         <Item key={product.id} product={product} />
       ))}
